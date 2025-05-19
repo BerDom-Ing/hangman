de82b032749cd9afa24f3f263865d32a3fc54b90
Bernardo Dominguez <ber.dom.engineer@gmail.com>
2025-05-19 18:07:46 -0300

    add lives system

--- app/game.py
+++ app/game.py
@@ -1,10 +1,15 @@
 class HangmanGame:
-    def __init__(self, word):
+    def __init__(self, word, lifes=6):
+        self.lifes = lifes
         self.word = word.lower()
         self.guessed_letters = set()
 
     def guess(self, letter):
-        self.guessed_letters.add(letter.lower())
+        letter = letter.lower()
+        self.guessed_letters.add(letter)
+        if letter not in self.word:
+            self.lifes -= 1
 
     def get_display_word(self):
         return ' '.join([letter if letter in self.guessed_letters else "_" for letter in self.word])
+
--- tests/test_game.py
+++ tests/test_game.py
@@ -8,3 +8,22 @@ def test_guess_correct_letter():
     game = HangmanGame("python")
     game.guess("p")
     assert game.get_display_word() == "p _ _ _ _ _"
+
+def test_guess_incorrect_letter():
+    game = HangmanGame("python")
+    game.guess("a")
+    assert game.get_display_word() == "_ _ _ _ _ _"
+
+def test_remaining_lifes():
+    game = HangmanGame("python", 5)
+    assert game.lifes == 5
+    game.guess("a")
+    assert game.lifes == 4
+    game.guess("b")
+    assert game.lifes == 3
+    game.guess("c")
+    assert game.lifes == 2
+    game.guess("d")
+    assert game.lifes == 1
+    game.guess("e")
+    assert game.lifes == 0
